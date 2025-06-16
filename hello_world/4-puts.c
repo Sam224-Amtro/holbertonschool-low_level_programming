7328af02461d60eaf05d7e65f895d756726d92d0
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-16 11:57:06 +0000

    file to the puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main print - this is like building a multilingual puzzle
+ *
+ * Return : always 0
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle"\n)
+		
+return (0);		
+}
