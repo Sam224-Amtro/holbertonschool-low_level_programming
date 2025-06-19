0b74ae2769d51f1fa23711c59583e66060ed24f0
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-19 22:40:08 +0000

    file to the add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - the unction that adds two integers and returns the result
+ * @a: The first number to be compiter
+ * @b: The second number to be compiter
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
