4ab4d5ee8713f6486cdc1bb910f321f3b3b135dc
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-20 12:56:46 +0000

    file to the time_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,10 +8,34 @@
  */
 void times_table(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		printf("9 * %d = %d\n", i, 9 * i);
+		for (j = 0; j <= 9; j++)
+		{
+			int result = i * j;
+
+			if (result < 10)
+			{
+				if (j != 0)
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
 	}
 }
