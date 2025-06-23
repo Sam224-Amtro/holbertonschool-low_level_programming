cd8518ad5c93d245fb96a8b40e79997c97e5b932
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-23 12:45:13 +0000

    file to the more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -17,17 +17,9 @@ void more_numbers(void)
 		for (b = 0; b < 15; b++)
 		{
 			if (b > 9)
-			{
-				_putchar(b / 10 + '0');
-
-				_putchar(b % 10 + '0');
-			}
-			else
-			{
-				_putchar(b + '0');
-			}
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
-
 	}
 }
