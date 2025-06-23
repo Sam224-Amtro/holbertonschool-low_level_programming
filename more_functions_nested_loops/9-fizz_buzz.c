9e792b10500b2ec9e522e3414bb86d10b8268113
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-23 20:47:28 +0000

    file to the Fizz-buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * mian -  that prints the numbers from 1 to 100
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0 && num != 100)
+		{
+			printf("Buzz ");
+		}
+		else if (num == 100)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
+	return (0);
+}
