2f95b84a532c90ca2e994e0fee921b8cc14ccce6
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-24 09:15:44 +0000

    file to_98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * reset_to_98 - that takes a pointer to an int as paramete
+ * and updates the value it points to to 98
+ * @n: The numbers to check
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Welcome to the projet C - Pointers, arrays and strings
