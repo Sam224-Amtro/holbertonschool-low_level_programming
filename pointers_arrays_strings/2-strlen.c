72a312e7a9f63cd80144fa7ca624b99f6850a8f6
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-24 12:03:20 +0000

    file to the strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - that returns the length of a string
+ * @s: the pointers in point
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return len;
+}
