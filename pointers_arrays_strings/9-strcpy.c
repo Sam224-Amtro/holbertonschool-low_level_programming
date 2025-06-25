e38f491fd96a9916ad0bd93e49105dd23a3438cb
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-25 17:14:52 +0000

    file to the strcopy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,20 +4,19 @@
 
 /**
  * _strcpy - Copies a string pointed to by @src, including the
- *           terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to src.
  * @src: The source string to copy.
  *
- * Return: A pointer to the destination string @dest.
+ * Return: Always 0
  */
 char *_strcpy(char *dest, char *src)
 {
-	int position;
+	int index;
 
-	while (src[position])
+	while (src[index])
 	{
-		dest[position] = src[position];
-		position++;
+		dest[index] = src[index];
+		index++;
 	}
 	return (dest);
 }
