35ef86eed0402ef3ccd102151696e0e05d6389bc
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-25 17:12:14 +0000

    file to the strcopy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 
 /**
- * _strcpy - that copies the string pointed to by src
- * @src: this is first pointers
- * @dest: this is second pointers
+ * _strcpy - Copies a string pointed to by @src, including the
+ *           terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to src.
+ * @src: The source string to copy.
  *
- * Return: Always 0
+ * Return: A pointer to the destination string @dest.
  */
 char *_strcpy(char *dest, char *src)
 {
