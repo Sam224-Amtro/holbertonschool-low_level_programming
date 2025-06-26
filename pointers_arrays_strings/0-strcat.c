b4b0baedbcd61e0bdb2a0f4bd7665d7b250a81c4
Sam224-Amtro <fofanasanoussy118@gmail.com>
2025-06-26 23:16:04 +0000

    file to the strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strcat - that concatenates two strings
+ * @src: The first parameter
+ * @dest: The second parameter
+ *
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int k;
+	int j;
+
+	for (k = 0; src[k] != '\0'; k++)
+	{
+		for (j = 0; dest[j] != '\0'; j++)
+		{
+			src[k] = dest[j];
+			k++;
+		}
+	}
+	return (dest);
+}
