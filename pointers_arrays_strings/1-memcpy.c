f1db5e46c7c0ec58f1023896f7d5c912d5ee4e28
Samamtro <fofanasanoussy545@gmail.com>
2025-06-30 11:10:11 +0200

    file to the 1-memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_memcpy - that copies memory area
+ * @n: the bystes from memory area
+ * @src: the source buffer
+ * @dest: the destimation buffer
+ *
+ *
+ * Return: A pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+		src[k] = dest[k];
+	}
+	return (dest);
+}
