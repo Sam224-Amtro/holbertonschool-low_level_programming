72b2df15c2828325abe50bf497dff6e6cbbf848a
Samamtro <fofanasanoussy545@gmail.com>
2025-07-02 19:01:22 +0200

    file to the is_prime_number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,40 +1,32 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
 
+int check_prime(int, int);
 /**
- * is_prime - hat returns 1 if the input integer is a prime number,
- * otherwise return 0
- * @n is number
+ * is_prime_number -> prime identifier
+ * @n: is number
  *
- * Return: Always 0
+ * Return: integer
  */
 int is_prime_number(int n)
 {
-	return (is_prime(n, 1));
+	return (check_prime(n, 1));
 }
+
+
 /**
- * is_prime - hat returns 1 if the input integer is a prime number,
- * otherwise return 0
- * @n: the number to check
+ * check_prime -> it check prime numbers
+ * @n: number
  * @i: iterator
  *
- * Return: Always 0 and 1
+ * Return: a 1 or 0
  */
 int check_prime(int n, int i)
 {
 	if (n <= 1)
-	{
 		return (0);
-	}
 	if (n % i == 0 && i > 1)
-	{
 		return (0);
-	}
 	if ((n / i) < i)
-	{
 		return (1);
-	}
 	return (check_prime(n, i + 1));
-
 }
