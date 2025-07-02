ca1404b02b8f2fc658a4d8d3e112210333cf9ab8
Samamtro <fofanasanoussy545@gmail.com>
2025-07-02 11:39:00 +0200

    file to the _pow_recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -14,11 +14,10 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 	{
 		return (1);
-		return (_pow_recursion(x, y + 1) / 2);
 	}
 	if (y < 1)
 	{
 		return (- 1);
 	}
-	return (_pow_recursion(x, y - 1) * x);
+	return (x * _pow_recursion(x, y - 1));
 }
