fa05cfe5da0cebb72bfcf6fbbdd9033744b0666d
Samamtro <fofanasanoussy545@gmail.com>
2025-07-03 12:30:52 +0200

    file to the add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main -Entry point
+ * @argc: this is argument
+ * @argv: the table to character
+ *
+ * Return: Always error, success 1
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int k, num;
+	char *endptr;
+
+	for (k = 1; k < argc; k++)
+	{
+		num = strtol(argv[k], &endptr, 10);
+		if (*endptr != '\0' || num < 0)
+		{
+			printf("Errror\n");
+			return (1);
+		}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
