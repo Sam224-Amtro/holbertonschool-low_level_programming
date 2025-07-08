f39c4880bde98cee028bb3d0c618b2f6e0aeb73f
Samamtro <fofanasanoussy545@gmail.com>
2025-07-08 13:45:20 +0200

    file to the malloc_checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - that allocates memory using malloc
+ * @b: this is number from byste
+ *
+ * Return: a pointer to the allocated memory, Else if return exit status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *k;
+
+	k = malloc(b);
+
+	if (k == NULL)
+	{
+			exit(98);
+	}
+	return (k);
+}
---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Welcome to the projet C - More malloc, free
