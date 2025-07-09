ea5b84d3c5e6e84c4ff723d0a06b8e7409adc1ef
Samamtro <fofanasanoussy545@gmail.com>
2025-07-09 12:03:54 +0200

    file to the print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name : %s\n", (d->name != NULL) ? d->name : "(NIL)");
-	printf("Age : %f\n", d->age);
+	printf("Age : %.6f\n", d->age);
 	printf("Owner : %s\n", (d->owner != NULL) ? d->owner : "(NIL)");
 }
