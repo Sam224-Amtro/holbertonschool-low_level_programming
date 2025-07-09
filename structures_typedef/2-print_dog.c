2b6391559a71df6cfef6e8d091c61246dfe237b2
Samamtro <fofanasanoussy545@gmail.com>
2025-07-09 17:42:12 +0200

    file to  the print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,10 +10,9 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)	/* Vérifie si le pointeur d est NULL */
+	if (d == NULL)
 		return;
 
-	/*Vérifie et affiche les informations avec gestion des NULL */
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
