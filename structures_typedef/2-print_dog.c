ccc522fe993b1cc56589cfad5d5c61e3776f375d
Samamtro <fofanasanoussy545@gmail.com>
2025-07-09 11:34:35 +0200

    file to the print_dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - that prints a struct dog
+ * @d: Pointer on the DOG structure to initialize
+ *
+ * Return: Always 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	printf("Name %s\n", d->name);
+	printf("Age %f\n", d->age);
+	printf("Owner %.3s\n", d->owner);
+}
