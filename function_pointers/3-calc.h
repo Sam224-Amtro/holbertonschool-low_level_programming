74c1d8f3b8553b90e1b6ea7ec34b9222f5a822a9
Samamtro <fofanasanoussy545@gmail.com>
2025-07-15 19:40:04 +0200

    file to the function multiple

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -8,11 +8,19 @@
 #include <ctype.h>
 #include <stddef.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+int (*get_op_func(char *s))(int, int);
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
