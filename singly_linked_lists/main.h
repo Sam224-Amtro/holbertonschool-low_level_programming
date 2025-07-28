17364c2c0bcf2d53d49d9576e587d30f720d7489
Samamtro <fofanasanoussy545@gmail.com>
2025-07-28 16:05:07 +0200

    the file to print_list

--- singly_linked_lists/main.h
+++ singly_linked_lists/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "lists.h"
+#include "main.h"
+#include <string.h>
+
+int _putchar(char c);
 
 #endif /* MAIN_H */
