653dab1764c72eb8795df5214f8a925024efb89e
Samamtro <fofanasanoussy545@gmail.com>
2025-07-29 10:01:14 +0200

    the lists to change

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - that adds a new node at the end of a list_t list
+ * @str: needs to be duplicated
+ * @head: this is the double pointers to check
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str){}
