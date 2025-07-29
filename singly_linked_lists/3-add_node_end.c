8a82f8612e7b6993f98ec2d033a4a9474af702f9
Samamtro <fofanasanoussy545@gmail.com>
2025-07-29 17:43:18 +0200

    the file to the add_node _end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,9 +12,11 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node;
-	list_t *last = *head;
+	list_t *new_node, *temp;
 	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
 
 	while (str[len])
 		len++;
@@ -29,7 +31,6 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->len = len;
 	new_node->next = NULL;
 
@@ -39,9 +40,11 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (new_node);
 	}
 
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new_node;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new_node;
 
 	return (new_node);
 }
